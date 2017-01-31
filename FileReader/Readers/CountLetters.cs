ad26e8cd7aa431fab6f3202ecc82aa0568024ff5
kendal <kendaltuanis@hotmail.com>
2017-01-30 20:27:41 -0600

    implementation

---
+++ FileReader/Readers/CountLetters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileReader.Readers
+{
+    class CountLetters : IReader
+    {
+        public string Name()
+        {
+            return "txt";
+        }
+
+        public string Read(string path)
+        {
+            int cont = 0;
+
+            String cadena = File.ReadAllText(path);
+            foreach (char item in cadena)
+            {
+                if (char.IsLetter(item))
+                {
+                    cont++;
+                }
+
+            }
+
+            return cont.ToString();
+
+        }
+    }
+}
