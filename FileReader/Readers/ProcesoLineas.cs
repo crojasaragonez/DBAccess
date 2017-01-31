8b16134850e9bf5d5eb6c9d48586d1b951d2f279
KevinArce98 <aa-kevin@hotmail.com>
2017-01-30 20:34:19 -0600

    Base64 contarLineas

---
+++ FileReader/Readers/ProcesoLineas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReader.Readers
+{
+    class ProcesoLineas
+    {
+        public static string contarLineas(String path)
+        {
+            StreamReader sr = new StreamReader(path);
+            String linea = "";
+            int i = 0;
+
+            while (sr.Peek() != -1)
+
+            {
+
+                sr.ReadLine();
+
+                i++;
+
+            }
+            linea = "El archivo contiene " + i + " lineas.";
+
+            return linea;
+        }
+
+    }
+}
