2b7d957a021200a46e77fa0f67970eadaa9d5b0c
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2017-01-30 19:03:11 -0800

    first reader

--- FileReader/Program.cs
+++ FileReader/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FileReader.Readers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace FileReader
     {
         static void Main(string[] args)
         {
+            IReader oReaader = new PlainTextReader();
+            Console.WriteLine(oReaader.Read("mi_archivo.txt"));
+
+            Console.ReadLine();
         }
     }
 }
