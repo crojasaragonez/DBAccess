6dbc74833f30514d4edfca1e982ef288d5a26a46
Kevin Arias <Kevin Arias>
2017-02-21 00:08:39 -0600

    MSAccess

--- DBAccess/MsAccess.cs
+++ DBAccess/MsAccess.cs
@@ -1,36 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+
 
 namespace DBAccess
 {
     class MsAccess : DBAccess
     {
+        private DataSet set;
+        private OleDbConnection con;
         public MsAccess(string connectionString) : base(connectionString)
         {
-
+            set = new DataSet();
         }
         public override void Connect()
         {
-            throw new NotImplementedException();
+            this.CleanStatus();
+            ConnectionStringSettings conSettings;
+            try
+            {
+                conSettings = ConfigurationManager.ConnectionStrings["MyAccessDBConnection"];
+                string connectionString = conSettings.ConnectionString;
+                con = new OleDbConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                this.ProcessException(ex);
+            }
+           
         }
 
         public override void Disconnect()
         {
-            throw new NotImplementedException();
+            this.CleanStatus();
+            try
+            {
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                this.ProcessException(ex);
+            }      
         }
 
         public override DataTable SqlQuery(string sql)
         {
-            throw new NotImplementedException();
+            this.CleanStatus();
+            DataTable retorno = null;
+            try
+            {
+                OleDbDataAdapter dat = new OleDbDataAdapter(sql, con);
+                dat.Fill(retorno);
+            }
+            catch (Exception ex)
+            {
+                this.ProcessException(ex);
+            } 
+            return retorno;
         }
 
         public override void SqlStatement(string pSql)
         {
-            throw new NotImplementedException();
+            this.CleanStatus();
+            try
+            {
+                OleDbCommand CmdSql = new OleDbCommand(pSql, con);
+                CmdSql.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                this.ProcessException(ex);
+            }
         }
     }
 }
