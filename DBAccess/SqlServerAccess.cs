4cce30934486e21d24394ac625e1b9985fc83fb8
kendal <kendaltuanis@hotmail.com>
2017-02-20 20:42:14 -0600

    SqlServerAccess DB

--- DBAccess/SqlServerAccess.cs
+++ DBAccess/SqlServerAccess.cs
@@ -1,36 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DBAccess
 {
-    class SqlServerAccess : DBAccess
+    public class SqlServerAccess : DBAccess
     {
+
+        private String connectionString;
+        private SqlConnection conn;
+
         public SqlServerAccess(string connectionString) : base(connectionString)
         {
-
+            this.connectionString = connectionString;
         }
         public override void Connect()
         {
-            throw new NotImplementedException();
+            conn = new SqlConnection();
+            conn.ConnectionString = this.ConnectionString();
+            try
+            {
+                this.CleanStatus();
+                conn.Open();
+
+            }
+            catch (Exception e)
+            {
+                ProcessException(e);
+            }
+
         }
 
         public override void Disconnect()
         {
-            throw new NotImplementedException();
+            try
+            {
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                ProcessException(e);
+            }
+
         }
 
         public override DataTable SqlQuery(string sql)
         {
-            throw new NotImplementedException();
+            DataTable data = new DataTable();
+
+            try
+            {
+                this.CleanStatus();
+                SqlCommand result = new SqlCommand(sql, conn);
+                data.Load(result.ExecuteReader());
+            }
+            catch (Exception e)
+            {
+                ProcessException(e);
+            }
+            return data;
         }
 
         public override void SqlStatement(string pSql)
         {
-            throw new NotImplementedException();
+            try
+            {
+                this.CleanStatus();
+                SqlCommand result = new SqlCommand(pSql, conn);
+                result.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                ProcessException(e);
+            }
+        }
+
+
+        private String ConnectionString()
+        {
+            String[] datos = connectionString.Split(',');
+
+            return String.Format("data source={0};initial catalog={1};user id={2};password={3}",
+                    datos[0],
+                    datos[1],
+                    datos[2],
+                    datos[3]
+            );
         }
     }
 }
