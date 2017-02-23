71a1d9def26df639793f1a1d79e1d857a36e56f8
Estudiante <estudiante@pssc.utn.ac.cr>
2017-02-23 17:58:06 -0600

    'OscaryVladimir'

--- DBAccess/MySqlAccess.cs
+++ DBAccess/MySqlAccess.cs
@@ -1,36 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MySql.Data.MySqlClient;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace DBAccess
 {
-    class MySqlAccess : DBAccess
+    public class MySqlAccess : DBAccess
     {
+
+        private MySqlConnection connection;
         public MySqlAccess(string connectionString) : base(connectionString)
         {
-
+            connection = new MySqlConnection(connectionString);
         }
         public override void Connect()
         {
-            throw new NotImplementedException();
+            if (this.connection.State == ConnectionState.Open) return;
+            try
+            {
+                connection.Open();
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
         }
 
         public override void Disconnect()
         {
-            throw new NotImplementedException();
+            try
+            {
+                connection.Close();
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
         }
 
         public override DataTable SqlQuery(string sql)
         {
-            throw new NotImplementedException();
+            MySqlDataAdapter oDataAdapter = new MySqlDataAdapter(sql, this.connection);
+            DataTable result = new DataTable();
+            result.Locale = CultureInfo.InvariantCulture;
+            try
+            {
+                oDataAdapter.Fill(result);
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+
+            return result;
         }
 
         public override void SqlStatement(string pSql)
         {
-            throw new NotImplementedException();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(pSql, this.connection);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+            
         }
     }
 }
